7dd832f3fedfc667d1e97d60d6d3f997b25b6b0f
Luiscrdzm <luiscarlos.rodriguezm@udem.edu>
2024-06-30 23:57:27 -0600

    Placeholder Index

--- mian.py
+++ mian.py
@@ -3,7 +3,9 @@ from flask import request, render_template, Flask
 app = Flask(__name__)
 
 
-
+@app.route('/')
+def index():
+    return "En la granja de cepillin habia una vaca, una vaca, una vaca y una vaca."
 
 
 
